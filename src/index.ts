f56a863e6946741ce6869da564554a44b8b2d972
Rhino <andre.bazoli@yara.com>
2021-09-04 10:42:47 +0200

    feat(troops): initial logic to generate troops

---
+++ src/index.ts
@@ -0,0 +1,25 @@
+function getTroopPercentage(troopSize: number) {
+  const percentage = Math.random() * 100 + 1;
+  return Math.floor((troopSize * percentage) / 100);
+}
+
+function getArm(troopSize: number, availableTroopTypes: Array<string>) {
+  if (troopSize <= 0) {
+    throw new Error(`Can't create arm with no man/woman`);
+  }
+  if (troopSize < 3) {
+    throw new Error(`Need at least 3 people to create an arm`);
+  }
+
+  const spearmen = getTroopPercentage(troopSize);
+  const swordsmen = getTroopPercentage(troopSize - spearmen);
+  const archers = troopSize - swordsmen - spearmen;
+  console.log(spearmen + swordsmen + archers);
+  return {
+    spearmen,
+    swordsmen,
+    archers,
+  };
+}
+console.clear();
+console.log(getArm(100, ["spearmen", "swordsmen", "archers"]));
