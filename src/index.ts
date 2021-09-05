ef2aac1515460aa2d04ea351853ae65452446565
Rhino <andre.bazoli@yara.com>
2021-09-05 10:05:12 +0200

    feat: make getArm generate units for any amount of unit types

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,11 @@ function getTroopPercentage(troopSize: number) {
   return Math.round((troopSize * percentage) / 100);
 }
 
+const isLastUnitType = (
+  availableTroopTypesAmount: number,
+  currentIndex: number
+) => availableTroopTypesAmount - 1 === currentIndex;
+
 export function getArm(
   troopSize: number,
   availableTroopTypes: Array<string>
@@ -22,15 +27,22 @@ export function getArm(
     );
   }
 
-  const spearmen = getTroopPercentage(troopSize);
-  const swordsmen = getTroopPercentage(troopSize - spearmen);
-  const archers = troopSize - swordsmen - spearmen;
-  console.log(spearmen + swordsmen + archers);
-  return {
-    spearmen,
-    swordsmen,
-    archers,
-  };
+  // remove the min amount of units to have at least one unit per type
+  let remainingTroopsUnit = troopSize - availableTroopTypesAmount;
+  return availableTroopTypes.reduce((troop: Troop, unitType, index) => {
+    // if it's the last one
+    if (isLastUnitType(availableTroopTypesAmount, index)) {
+      troop[unitType] = 1 + remainingTroopsUnit;
+      return troop;
+    }
+
+    const unitAmount = getTroopPercentage(remainingTroopsUnit);
+    remainingTroopsUnit -= unitAmount;
+    // Make sure all units will have at least one unit
+    troop[unitType] = 1 + unitAmount;
+    return troop;
+  }, {});
 }
+
 console.clear();
 console.log(getArm(100, ["spearmen", "swordsmen", "archers"]));
