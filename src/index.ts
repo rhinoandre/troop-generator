47b0514328b52fc078851917e183ba27e800a213
Rhino <andre.bazoli@yara.com>
2021-09-05 10:01:05 +0200

    fix(validation): use amount of unit types in the validation

--- src/index.ts
+++ src/index.ts
@@ -14,8 +14,12 @@ export function getArm(
   if (troopSize <= 0) {
     throw new Error(`Can't create arm with no man/woman`);
   }
-  if (troopSize < 3) {
-    throw new Error(`Need at least 3 people to create an arm`);
+
+  const availableTroopTypesAmount = availableTroopTypes.length;
+  if (troopSize < availableTroopTypesAmount) {
+    throw new Error(
+      `Need at least ${availableTroopTypesAmount} people to create an arm`
+    );
   }
 
   const spearmen = getTroopPercentage(troopSize);
